4eebb3bace93a76c489741b04a368c185dedfd70
Nathan Rose <contacttstyle@gmail.com>
2025-03-26 01:30:45 -0500

    Update large-testimonial.tsx

--- components/large-testimonial.tsx
+++ components/large-testimonial.tsx
@@ -30,18 +30,15 @@ export default function LargeTestimonial() {
               />
             </div>
             <p className="text-2xl font-bold text-gray-900">
-              “Simple has simplified my life in more ways than one. From
-              managing my sites to{" "}
-              <em className="italic text-gray-500">keeping track of tasks</em>,
-              it's become my go-to tool for everything.”
+            "The Steel Law Firm's defense strategy was <em className="italic text-gray-500">absolutely brilliant</em>. Their command of the law and courtroom presence made all the difference in my case."
             </p>
-            <div className="text-sm font-medium text-gray-500">
-              <span className="text-gray-700">Mary Sullivan</span>{" "}
-              <span className="text-gray-400">/</span>{" "}
-              <a className="text-blue-500" href="#0">
-                CTO at Microsoft
-              </a>
-            </div>
+          <div className="text-sm font-medium text-gray-500">
+           <span className="text-gray-700">Jeffery Williams (Young Thug)</span>{" "}
+           <span className="text-gray-400">/</span>{" "}
+           <a className="text-blue-500" href="#0">
+            Recording Artist
+           </a>
+        </div>  
           </div>
         </div>
       </div>
