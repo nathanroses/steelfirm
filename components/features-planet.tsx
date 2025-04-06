02e7bee4963f484c1fbf67cd328a6f83dcce8e2e
Nathan Rose <contacttstyle@gmail.com>
2025-04-06 16:12:05 -0400

    Update features-planet.tsx

--- components/features-planet.tsx
+++ components/features-planet.tsx
@@ -14,7 +14,7 @@ export default function LegalPracticeAreas() {
           {/* Section header */}
           <div className="mx-auto max-w-3xl pb-16 text-center md:pb-20">
             <h2 className="text-3xl font-bold text-gray-200 md:text-4xl">
-              Comprehensive Legal Strategies For Complex Cases
+              Comprehensive Strategies For Complex Cases
             </h2>
           </div>
           {/* Legal Shield Visualization */}
